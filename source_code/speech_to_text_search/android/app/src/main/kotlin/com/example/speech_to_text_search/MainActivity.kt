c43e95705b5d1fa2b810076cd1094ce15a02f143
Partha9318 <parthaptalukdar7@gmail.com>
2025-04-10 13:49:54 +0530

    Repo shift

--- source_code/speech_to_text_search/android/app/src/main/kotlin/com/example/speech_to_text_search/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.speech_to_text_search
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- source_code/speech_to_text_search/lib/Service/api_constants.dart
+++
@@ -1,3 +0,0 @@
-const String baseUrl = 'https://probill.app/api';
-const String databaseUrl = '';
-// const String baseUrl = 'https://dev.probill.app/api';
